13aa2a2d574c458705fd3fcb63e85afb52f51b24
Olav Povo <olav@MacOlav.local>
2025-08-12 18:27:33 -0400

    portfolio.py: added Share Info menu, furthur explored Duration, else option in addShares

--- portfolio.py
+++ portfolio.py
@@ -1,24 +1,37 @@
 
-##Get stocks, and quantity##
+##Get stocks, quantity, cost to provide costprice.
+from datetime import datetime
 
 def addShares(prtf):
-    name=input("Enter symbol: ").upper()
+    name= "nvda".upper() #input("Enter symbol: ").upper()
     if name not in prtf:
         prtf[name]={}
         shares= 8 #int(input("Enter shares: "))
-        prtf[name]["shares"] = shares
+        prtf[name]["shares"] =  shares
         prtf[name]["cost"] = 10 #float(input("Enter cost: "))
         prtf[name]["costprice"]=prtf[name]["cost"]*shares
-        print(f"Added {shares} shares of {name} at {prtf[name]['cost']}, for a total value of {prtf[name]['costprice']}")
+        date=input("Date of Trade: YYYY.MM.DD ")
+        prtf[name]["dateAdded"]=datetime.strptime(date, "%Y.%m.%d")
+        print(f"{prtf[name]['dateAdded'].strftime('%Y.%m.%d')}: {shares} shares of {name} at {prtf[name]['cost']}, for a total value of {prtf[name]['costprice']}")
+    else:
+        print(f"{name} is already in portfolio {prtf[name]}.")
+#There is room above to improve code into a def def to organize data.
 
-prtf={}
-
+#Navigation Menu
 def start():
     print("")
     print("1. Add Stock")
+    print("2. Share Info")
     print("0. EXIT")
     print("")
 
+def shareInfo(prtf):
+    today=datetime.today()
+    for key,value in prtf.items():
+        print(key,value, f"Duration: {prtf[key]['dateAdded']-today}")
+
+
+prtf={}
 
 while True:
     start()
@@ -26,6 +39,8 @@ while True:
 
     if entry == 1:
         addShares(prtf)
+    elif entry == 2:
+        shareInfo(prtf)
     if entry == 0:
         break
 
