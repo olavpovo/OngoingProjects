c6a5e55d9da507c03233a63f8ba349c22e8617d8
Olav Povo <olav@MacOlav.local>
2025-08-12 16:18:39 -0400

    Improved addShares function and introducted navigation

--- portfolio.py
+++ portfolio.py
@@ -1,26 +1,36 @@
 
 ##Get stocks, and quantity##
 
-
-def addPosition():
-    ticker= "nvda".upper() #str(input("Please input ticker symbol: ")).upper()
-    if ticker not in prtf:
-        prtf["name"]= ticker
-        for key,values in prtf.items():
-            if values == ticker:
-                prtf["name"]["shares"]=30
-                print("done")
-
-
+def addShares(prtf):
+    name=input("Enter symbol: ").upper()
+    if name not in prtf:
+        prtf[name]={}
+        shares= 8 #int(input("Enter shares: "))
+        prtf[name]["shares"] = shares
+        prtf[name]["cost"] = 10 #float(input("Enter cost: "))
+        prtf[name]["costprice"]=prtf[name]["cost"]*shares
+        print(f"Added {shares} shares of {name} at {prtf[name]['cost']}, for a total value of {prtf[name]['costprice']}")
 
 prtf={}
-addPosition()
+
+def start():
+    print("")
+    print("1. Add Stock")
+    print("0. EXIT")
+    print("")
 
 
-def addShares(stkQuan):
-    quantity=float(input("How many shares?: "))
-    stkQuan.append(quantity)
-    price=float(input("At what price? "))
+while True:
+    start()
+    entry=int(input("Please type of #: "))
+
+    if entry == 1:
+        addShares(prtf)
+    if entry == 0:
+        break
+
+
+
 
 
 
